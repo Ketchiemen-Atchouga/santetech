44561b3756be40519274b4325880654f92b571aa
Ketchiemen-Atchouga <ketchiemenatchouga@gmail.com>
2026-04-23 20:53:51 +0100

    flix port railway

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def dashboard():
 def recherche():
     return redirect(url_for('dashboard'))
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0', port=port)
